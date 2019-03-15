dc70314eb9e67cb8f8bd273715d181cc73f52d51
Dustin Schau <dustinschau@gmail.com>
2019-03-15 11:09:54 -0500

    feat: fix

--- scripts/copy.js
+++ scripts/copy.js
@@ -3,16 +3,32 @@ const fs = require('fs-extra')
 
 const base = path.resolve('presentations')
 const destination = path.resolve('public')
-const publishDir = 'dist'
+const getPublishDirectory = dependencies => {
+  const lookup = {
+    'mdx-deck': 'dist',
+    'spectacle': 'public',
+    'spectacle-scripts': 'public',
+    undefined: 'dist'
+  }
+
+  const lib = Object.keys(lookup).find(lib => dependencies[lib])
+  return lookup[lib]
+}
 
 async function copy() {
   const presentations = await fs.readdir(base)
 
   await Promise.all(
-    presentations.map(presentation => {
-      const folder = path.join(base, presentation, publishDir)
+    presentations.map(async presentation => {
+      const folder = path.join(base, presentation)
 
-      return fs.copy(folder, 
+      const { dependencies } = await fs.readJson(path.join(folder, 'package.json'))
+      const publishDir = getPublishDirectory(dependencies)
+
+      console.log(publishDir)
+
+      return fs.copy(
+        path.join(folder, publishDir), 
         path.join(destination, presentation)  
       )
     })
