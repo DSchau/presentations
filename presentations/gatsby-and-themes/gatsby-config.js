4d999ecf387290635c506a527907bda3295db33d
Dustin Schau <dustinschau@gmail.com>
2019-04-24 22:30:02 +0300

    chore: actually fix

--- presentations/gatsby-and-themes/gatsby-config.js
+++ presentations/gatsby-and-themes/gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/gatsby-and-themes`,
   siteMetadata: {
     title: `Gatsby + Themes`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
